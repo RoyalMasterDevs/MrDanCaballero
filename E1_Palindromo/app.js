f5d5a62c39afe94e69177c9c96198b38981e5d8c
MrDanCaballero <dancab1@gmail.com>
2025-02-01 00:55:44 -0600

    algoritmo

--- E1_Palindromo/app.js
+++ E1_Palindromo/app.js
@@ -1,7 +1,18 @@
 //MrDanCaballero
+/**
+ * se recibe la palabra a revisar
+ * se descomone en caracteres
+ * se escribem los caracteres del final al inicio para invertir el orden
+ * se compara la palabra iniciar con la nueva escrita alrevez
+ * si es igual es palindromo
+ * si no no es palindromo
+ * 
+ * @param {} str 
+ * @returns 
+ */
 function palindromo(str) {
     //eliminar los espacios
-    var re = /[\W_]/g;
+    //var re = /[\W_]/g;
     var val = "No es palindromo"
     //pasar a minusculas
     var minusculas = str.toLowerCase().replace(re, '');
